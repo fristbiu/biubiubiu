a1d78ca789872b9f30d8d2b41f87cf76a00c7096
ren <ren@qq.com>
2017-07-21 09:34:35 +0800

    dsad

--- frontend/controllers/ShowController.php
+++ frontend/controllers/ShowController.php
@@ -9,12 +9,32 @@ namespace frontend\controllers;
 use Yii;
 use yii\helpers\Url;
 use backend\models\Profession_type;
+use frontend\models\Business;
+use frontend\models\Advertise;
 class ShowController extends CommonController{
 //    项目样式
     public $layout='common';
 //    首页
     public function actionIndex(){
-        return $this->render('index');
+    	//处理公司和简历的数据
+    	$business=Business::find()->asArray()->all();
+    	$advertise=Advertise::find()->limit(20)->asArray()->all();
+		//整合数据
+    	$newArr=$this->addinfo($business,$advertise);
+    	
+        return $this->render('index',array('newArr'=>$newArr));
+    }
+
+    public function addinfo($business,$advertise)
+    {
+    	foreach($advertise as &$val){
+    		foreach($business as $v){
+    			if($val['bussiness_id']==$v['bussiness_id']){
+    				$val['bussiness']=$v;
+    			}
+    		}
+    	}
+    	return $advertise;
     }
 
 
